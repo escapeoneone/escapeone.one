8ef55858f32162ee676dcb91ae6b5cfa90d9bfde
escapeoneone <boomik305@gmail.com>
2021-02-10 12:21:09 +0300

    work

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import styles from "./Header.module.css";
 import HeaderItem from "./HeaderItem";
+import Link from "next/link";
 
 export default function Header() {
     return (<>
         <header className={styles.header}>
             <nav className={styles["header-logo-nav"]}>
-                <a className={styles["header-logo"]} href="/">escapeoneone.</a>
+                <Link href="/">
+                    <a className={styles["header-logo"]}>escapeoneone.</a>
+                </Link>
             </nav>
             <nav>
                 <HeaderItem href="projects" text="My projects"></HeaderItem>
