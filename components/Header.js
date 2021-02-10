6560241ed474f8d997b73662b9c9472e7e5f7789
escapeoneone <boomik305@gmail.com>
2021-02-10 10:24:10 +0300

    Create fonts

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return (<>
+        <header className={styles.header}>
+            <nav className={styles["header-logo-nav"]}>
+                <a className={styles["header-logo"]} href="/">escapeoneone.</a>
+            </nav>
+            <nav>
+                <a className={styles["header-menu-item"]} href="/skills">Skills</a>
+            </nav>
+
+        </header>
+    </>)
+}
