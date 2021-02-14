6a31c8e3adc58608640395cf34b00f95fe632966
escapeoneone <boomik305@gmail.com>
2021-02-14 15:08:33 +0300

    add google analytic

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,16 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-54K6D9WN1F"></script>
+  <script>{
+    () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+  
+      gtag('config', 'G-54K6D9WN1F');
+    }
+}</script>
     <script data-ad-client="ca-pub-3624282771754825" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <Component {...pageProps} />
   </>
