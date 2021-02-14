f6a243d6af1749978222a46d4482ee33b61d646f
escapeoneone <boomik305@gmail.com>
2021-02-14 15:56:54 +0300

    add metric

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,30 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-54K6D9WN1F"></script>
-  <script>{
-    () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+  <div dangerouslySetInnerHTML={`
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-54K6D9WN1F"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+    
+        gtag('config', 'G-54K6D9WN1F');
+      </script>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
   
-      gtag('config', 'G-54K6D9WN1F');
-    }
-}</script>
+     ym(72405547, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+     });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/72405547" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->`}></div>
     <script data-ad-client="ca-pub-3624282771754825" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <Component {...pageProps} />
   </>
